d33ff0301e17f108984c5dd830601a5e3ea94b62
TL-Yao <31872143+TL-Yao@users.noreply.github.com>
2017-09-26 00:20:50 -0400

    ty.yao

--- sorter.c
+++ sorter.c
@@ -5,54 +5,59 @@
 #include <ctype.h>
 #include <string.h>
 
+
 typedef struct{
-	char **row;	
+    char** row_token;
+    char * row_text;
+    size_t row_size;
+    size_t num_col;
 }row;
 
 int main (int argc, char* argv[]){
+	/*start declare variable*/
 	FILE *fp;
 	fp = stdin;
-	char *first_line;
-	first_line = (char*) malloc (sizeof(char) * BUF_SIZE);
-	char *rest_line;
-	rest_line = (char*) malloc (sizeof(char) * BUF_SIZE);
-	char *token;
-	char ** first_line_token;
-	char ** rest_line_token;
-	size_t line_size;
-	size_t num_col = 1;
 
-	fgets(first_line, BUF_SIZE-1, fp);
-	line_size = strlen(first_line);
-	first_line_token = (char**) malloc(sizeof(char *) * line_size);
-	token = strtok(first_line, ",");
-	first_line_token[0] = token;
+	row first_row;
+	first_row.row_text = (char*) malloc (sizeof(char) * BUF_SIZE);
+	row rest_row;
+	rest_row.row_text = (char*) malloc (sizeof(char) * BUF_SIZE);
+
+
+	char *token;
+	size_t num_col = 1;
+	/*end of declare variable*/
+
+	/*deal with the first row which contains all titles.*/
+	fgets(first_row.row_text, BUF_SIZE-1, fp);
+	first_row.row_size = strlen(first_row.row_text);
+	first_row.row_token = (char**) malloc(sizeof(char *) * first_row.row_size);
+	token = strtok(first_row.row_text, ",");
+	first_row.row_token[0] = token;
 	
 	while(token = strtok(NULL, ",")){
-		//printf("%d token is: %s\n",num_col, token);
-		first_line_token[num_col++] = token;
-		//printf("inside %d token is: %s\n",num_col, first_line_token[num_col - 1]);
+		//printf("%d token is: %s\n",num_col, token);//debug
+		first_row.row_token[num_col++] = token;
+		//printf("inside %d token is: %s\n",num_col, first_row.row_token[num_col - 1]);//debug
 	}
 
-	free(first_line);
-
-	//fgets(rest_line, BUF_SIZE-1, fp);
-	while(fgets(rest_line, BUF_SIZE-1, fp) != NULL){
+	/*deal with the rest rows which contain all data.*/
+	while(fgets(rest_row.row_text, BUF_SIZE-1, fp) != NULL){
 		num_col = 1;
-		line_size = strlen(rest_line);
-		rest_line_token = (char**) malloc(sizeof(char *) * line_size);
-		token = strtok(rest_line, ",");
-		rest_line_token[0] = token;
+		rest_row.row_size = strlen(rest_row.row_text);
+		rest_row.row_token = (char**) malloc(sizeof(char *) * rest_row.row_size);
+		token = strtok(rest_row.row_text, ",");
+		rest_row.row_token[0] = token;
 		printf("%d token is: %s\n",num_col, token);
 		
 		while(token = strtok(NULL, ",")){
-			printf("%d token is: %s\n",num_col, token);
-			rest_line_token[num_col++] = token;
-			printf("inside %d token is: %s\n",num_col, rest_line_token[num_col - 1]);
+			//printf("%d token is: %s\n",num_col, token);//debug
+			rest_row.row_token[num_col++] = token;
+			//printf("inside %d token is: %s\n",num_col, rest_row.row_token[num_col - 1]);//debug
 		}
 	}
 
 	//printf("%d", first_line_size);
-	printf("%s", rest_line_token[0]);
+	//printf("%s", rest_row.row_token[0]);
 }
 
